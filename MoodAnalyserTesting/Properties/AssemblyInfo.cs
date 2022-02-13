18f4a611bad7082f2b66b2abee44977e2c0ca088
kiran04121997 <bhosalekiran259@gmail.com>
2022-02-13 19:25:18 +0530

    [kiran] ability to mood analyzer

---
+++ MoodAnalyserTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoodAnalyserTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("MoodAnalyserTesting")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85c924b2-f5cd-40d5-8535-2a7e7b745258")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MoodAnalyserTesting/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyserProject;
+using System;
+
+namespace MoodAnalyserTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMessageShouldReturnHappy()
+        {
+            ///Follow AAA strategy
+            ///Arrange , Act and in last Assert
+            AnalyzeMood mood = new AnalyzeMood();
+            string message = "I am in Happy Mood";
+            string excepted = "happy";
+            var actual = mood.Mood(message);
+            Assert.AreEqual(excepted, actual);
+        }
+        [TestMethod]
+        [TestCategory("SAD Mood")]
+        public void GivenMessageShouldReturnSad()
+        {
+            ///Follow AAA strategy
+            ///Arrange , Act and in last Assert
+            AnalyzeMood mood = new AnalyzeMood();
+            string message = "I am in SAD Mood";
+            string excepted = "sad";
+            var actual = mood.Mood(message);
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
